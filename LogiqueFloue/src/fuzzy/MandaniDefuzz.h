6475d13dbf76f56da3c2212169e6ad2188da91f0
Cédric Szymanski <szymanski.cedric@gmail.com>
2016-02-24 17:22:56 +0100

    Mandani

---
+++ LogiqueFloue/src/fuzzy/MandaniDefuzz.h
@@ -0,0 +1,36 @@
+/*
+ * MandaniDefuzz.h
+ *
+ *  Created on: 24 févr. 2016
+ *      Author: Szymanski
+ */
+
+#ifndef FUZZY_MANDANIDEFUZZ_H_
+#define FUZZY_MANDANIDEFUZZ_H_
+
+#include "../core/BinaryExpression.h"
+
+namespace fuzzy
+{
+
+template <class T>
+class MandaniDefuzz : core::BinaryExpression<T>
+{
+public:
+	MandaniDefuzz(const T&, const T&, const T&);
+	virtual ~MandaniDefuzz() {};
+
+	T evaluate(Expression<T>*, Expression<T>*) const;
+
+	virtual T defuzz() const = 0;
+
+private:
+
+	T min, max, step, current;
+};
+
+
+
+
+
+#endif /* FUZZY_MANDANIDEFUZZ_H_ */
